2af2b1eb6f17a3ca299ed70f5c3cce30a3219516
Hanbiike <bika@MacBook-Air-Hanbiike.local>
2025-02-24 01:55:35 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import SearchAppBar from "./components/searchBar/searchBar.tsx";
+import ShowDetails from "./pages/showDetails/showDetails.tsx";
+import { CssBaseline, Container } from "@mui/material";
+
+
+export default function App() {
+    return (
+        <>
+            <CssBaseline />
+            <Container>
+                <Routes>
+                    <Route path="/" element={<SearchAppBar />} />
+                    <Route path="/shows/:id" element={<ShowDetails />} />
+                </Routes>
+            </Container>
+        </>
+    );
+}
